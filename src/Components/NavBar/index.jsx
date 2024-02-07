d9d68c8118af657cc6cbfe588bf0b1bc9d8c4da3
ovass <vassarottowen@gmail.com>
2024-02-07 01:59:22 -0300

    Added account key and sign-out to localStorage

--- src/Components/NavBar/index.jsx
+++ src/Components/NavBar/index.jsx
@@ -5,7 +5,7 @@ import { FaShoppingBag } from "react-icons/fa";
 
 const NavBar = () => {
 
-    const { count, openCheckoutSideMenu, closeProductDetail, productsCategories } = useContext(ShoppingCartContext);
+    const { count, openCheckoutSideMenu, closeProductDetail, productsCategories, signOut, setSignOut } = useContext(ShoppingCartContext);
 
     /*
         "electronics",
@@ -13,6 +13,12 @@ const NavBar = () => {
         "men's clothing",
         "women's clothing"
     */ 
+
+    const handleSignOut = () => {
+        localStorage.setItem('sign-out', JSON.stringify(true));
+        setSignOut(true);
+    }
+
   return (
     <nav id="sidebar" className="flex justify-between items-center fixed top-0 shadow w-full z-10 py-5 px-8 text-sm font-light bg-slate-800">
         <ul className="flex items-center gap-3">
@@ -62,8 +68,9 @@ const NavBar = () => {
             <li>
                 <NavLink
                     to={"/sign-in"}
+                    onClick={() => handleSignOut()}
                 >
-                    Sign In
+                    Sign Out
                 </NavLink>
             </li>
             <button 
