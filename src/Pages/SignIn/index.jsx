fef3b857652b1b267793d1e3b587dedb09df83f3
ovass <vassarottowen@gmail.com>
2024-02-09 21:18:04 -0300

    Added functionality to determine whether a user has an account or not, enabling the display of user data and enabling/disabling buttons accordingly

--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -1,6 +1,18 @@
+import { useContext } from "react"
 import { Link } from "react-router-dom"
+import { ShoppingCartContext } from "../../context"
 
 const SignIn = () => {
+
+  const context = useContext(ShoppingCartContext);
+
+  // to verify if exists an account in local storage and local state
+  const existsAccountInLocalStorage = JSON.parse(localStorage.getItem('account')) ? Object.keys(JSON.parse(localStorage.getItem('account'))).length > 0 : false;
+  const existsAccountInLocalState = context.account ? Object.keys(context.account).length > 0 : false;
+  const hasUserAnAccount = existsAccountInLocalStorage || existsAccountInLocalState;
+ 
+  console.log(hasUserAnAccount);
+
   return (
     <>
       <h1 className="text-center mt-4 mb-6 text-2xl">Welcome</h1>
@@ -18,7 +30,10 @@ const SignIn = () => {
         </p>
 
         <Link to={"/"}>
-          <button className="bg-slate-950 hover:bg-slate-950/75 desabled:bg-slate-950/40 text-white w-full rounded-lg py-3 mt-4 mb-2">
+          <button 
+            className="bg-slate-950 hover:bg-slate-950/75 desabled:bg-slate-950/40 text-white w-full rounded-lg py-3 mt-4 mb-2"
+            disabled={hasUserAnAccount === false}
+          >
             Log In
           </button>
         </Link>
@@ -29,6 +44,7 @@ const SignIn = () => {
 
         <button
           className="border border-gray-600 disabled:text-white/40 disabled:border-gray/40 rounded-lg mt-4 py-3 hover:bg-slate-950/10"
+          disabled={hasUserAnAccount === true}
         >
           Sign Up
         </button>
